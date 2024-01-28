767f39ba09625fd135018639ef59bb7b3a1281c0
imcookiedabadee <ohitscookiemfl@gmail.com>
2024-01-28 11:11:34 -0500

    fixed bug
    :bug:

--- Assets/FollowPlayer.cs
+++ Assets/FollowPlayer.cs
@@ -17,6 +17,7 @@ public class FollowPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        nav.destination = player.transform.position;
+        if(player)
+            nav.destination = player.transform.position;
     }
 }
